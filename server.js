660bad04e6341c4d99525b816a0f10751a3d4846
favioloshow <faviocuyazevallos@gmail.com>
2021-11-05 17:22:45 -0500

    12

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./inventory-app'));
+app.use(express.static('./dist/inventory-app/'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/inventory-app/'}),
